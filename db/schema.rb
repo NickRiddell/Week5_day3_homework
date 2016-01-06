02a353cdad165328f97e3ea809c5264e2b0d2855
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-06 15:33:21 +0000

    added comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106142957) do
+ActiveRecord::Schema.define(version: 20160106153232) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20160106142957) do
   end
 
   create_table "authors", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "content"
     t.integer  "user_id"
     t.integer  "article_id"
     t.datetime "created_at", null: false
