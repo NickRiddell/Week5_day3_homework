337a0834b71eb327f6225098016551907d64906a
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-06 14:31:03 +0000

    added associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106115632) do
+ActiveRecord::Schema.define(version: 20160106142957) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
