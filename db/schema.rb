11e0bd64ed20a7502eb8ad4ed1c91fdbe8b3c616
Nick Riddell <nickjriddell@gmail.com>
2016-01-06 21:12:21 +0000

    Done playing with CSS for the night

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106153232) do
+ActiveRecord::Schema.define(version: 20160106193100) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160106153232) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.text     "image"
   end
 
   create_table "authors", force: :cascade do |t|
